a7b22d88571017c9d8a91560897fa3b49a5b5fa0
woooooooojin <kevin2205@naver.com>
2023-12-14 15:40:10 +0900

    css

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,10 @@
 export default function Home() {
   return (
     <>
-      <h2>welcome</h2>
+      <h2 className="h2">welcome</h2>
       hello web<br/><br/>
       <img src="/next.svg"></img>
+
     </>  
   )
 }
